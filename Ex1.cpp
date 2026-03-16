a6489af283fc9d8945ceda18e07376c149a29c67
Nicolas Lemes <nicolaslemes2004@gmail.com>
2026-03-16 07:42:28 -0300

    Refactor difference calculation and output logic

--- Ex1.cpp
+++ Ex1.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>;
 
 int main(){
-  float n1, n2, dif;
+  float n1, n2, ,ma , me , dif;
   printf("Digite um valor: ");
   scanf("%f", &n1);
 
@@ -11,10 +11,17 @@ int main(){
 
   if(n1 > n2){
     dif = n1 - n2;
-    printf("A diferença do maior num %f pelo menor %f eh %f", n1, n2, dif);
+    ma = n1;
+    me = n2;
   }else{
     dif = n2 - n1;
-    printf("A diferença do maior num %f pelo menor %f eh %f", n1, n2, dif);
+    ma = n2;
+    me = n1;
+  }
+  if (n1 == n2){
+    printf("A diferença é 0");
+  }else{
+     printf("A diferença do maior num %f pelo menor %f eh %f", ma, me, dif);
   }
 
   return 0;
